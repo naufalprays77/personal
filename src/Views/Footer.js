17d6d10a49adf3c0758a0173a3e6204d59c1d6db
nanda.prayasdewo <nanda.prayasdewo@gmail.com>
2022-06-29 20:38:39 +0700

    Add Logo in Footer

--- src/Views/Footer.js
+++ src/Views/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Logo from '../Assets/Img/NPLogo1.1.png';
 
 function Footer() {
   return (
     <footer className="contact bg-black" id="contact">
       <div className="text-center text-white py-10">
         <h1>
-          <br /> Develop 2022 by
+          <img src={Logo} className="lg:w-[3%] md:w-[5%] w-[13%] mx-auto pb-2" />
+          Develop 2022 by
           <a href="https://www.instagram.com/naufalprays77/" className="hover:text-blueSoft px-1">
             @naufalprays77
           </a>
